6b27f13604b270cede8afe6b2dfbf50248db5b43
Deep Coder <deep@pop-os.localdomain>
2020-02-21 12:56:46 +0530

    Changes for using webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "file-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: "./public/index.html",
+          filename: "./index.html"
+        })
+      ]
+};
